8d17098a19194a779ff7a765be840809569df0b1
damn21 <dmezae@uninorte.edu.co>
2021-11-21 02:58:15 +0000

    feat: :bank: migrations tweet

---
+++ src/database/models/tweet.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-param-reassign */
+const {
+  Model,
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Tweet extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Tweet.init({
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    likeCounter: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    idUser: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  }, {
+    sequelize,
+    modelName: 'Tweet',
+  });
+  return Tweet;
+};
